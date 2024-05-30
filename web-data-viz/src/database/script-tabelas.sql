41c37e03c51b9627bdb9ada8f71d86a647753866
NaomiCalisto <naomi.calisto@sptech.school>
2024-05-30 18:41:43 -0300

    Aplicação da API web-data-viz

---
+++ web-data-viz/src/database/script-tabelas.sql
@@ -0,0 +1,25 @@
+-- Arquivo de apoio, caso você queira criar tabelas como as aqui criadas para a API funcionar.
+-- Você precisa executar os comandos no banco de dados para criar as tabelas,
+-- ter este arquivo aqui não significa que a tabela em seu BD estará como abaixo!
+
+/*
+comandos para mysql server
+*/
+
+create database astro;
+use astro;
+
+create table Usuario(
+	idUsuario int primary key auto_increment,
+	nome varchar(45),
+	sobrenome varchar(45),
+	email varchar(45),
+		constraint chkEmail check (email like '%\%%'),
+	diaInscricao datetime default current_timestamp);
+    
+create table Scoreboard(
+	idScoreboard int primary key auto_increment,
+    score int,
+    fkUsuario int,
+		foreign key (fkUsuario) references Usuario(idUsuario));
+    
